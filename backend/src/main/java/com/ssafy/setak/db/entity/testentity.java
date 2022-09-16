46357eef96d90c5d7150dde4d14b17808c8c44bf
1upright <kompany11@hanmail.net>
2022-09-17 00:01:08 +0900

    settings: update build.gradle

--- backend/src/main/java/com/ssafy/setak/db/entity/testentity.java
+++ backend/src/main/java/com/ssafy/setak/db/entity/testentity.java
@@ -1,4 +1,17 @@
 package com.ssafy.setak.db.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class testentity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "test_id")
+    private Long id;
 }
