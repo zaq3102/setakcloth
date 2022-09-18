387c273a80345be87c786319dd93af1893ad930b
마유선 <jasminema@naver.com>
2022-09-18 21:58:53 +0900

    feat: update ceowallet repository - change entity type

--- backend/src/main/java/com/ssafy/setak/db/repository/CeoWalletRepository.java
+++ backend/src/main/java/com/ssafy/setak/db/repository/CeoWalletRepository.java
@@ -1,9 +1,10 @@
 package com.ssafy.setak.db.repository;
 
 import com.ssafy.setak.db.entity.CeoUser;
+import com.ssafy.setak.db.entity.CeoWallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CeoWalletRepository extends JpaRepository<CeoUser, Long> {
+public interface CeoWalletRepository extends JpaRepository<CeoWallet, Long> {
 }
