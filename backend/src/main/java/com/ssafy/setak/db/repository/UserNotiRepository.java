491d7daa2903a062156e817cd3183ec71597dceb
마유선 <jasminema@naver.com>
2022-09-18 22:00:12 +0900

    feat: add usernoti repository

---
+++ backend/src/main/java/com/ssafy/setak/db/repository/UserNotiRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.setak.db.repository;
+
+import com.ssafy.setak.db.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserNotiRepository extends JpaRepository<Order, Long> {
+}
