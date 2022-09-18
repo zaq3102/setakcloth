97e5a2be46a3589d9a84d7bd1f3f196aabc60c70
마유선 <jasminema@naver.com>
2022-09-18 20:21:26 +0900

    feat: add userwallet entity

---
+++ backend/src/main/java/com/ssafy/setak/db/entity/UserWallet.java
@@ -0,0 +1,23 @@
+package com.ssafy.setak.db.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserWallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_wallet_id")
+    private Long id;
+
+    private String walletAddr;
+    private float balance;
+
+}
