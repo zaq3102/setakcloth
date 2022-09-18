11759ca952015cb4531c970c312d9dbc4ad194fc
마유선 <jasminema@naver.com>
2022-09-18 20:20:52 +0900

    feat: add orderdetail entity

---
+++ backend/src/main/java/com/ssafy/setak/db/entity/OrderDetail.java
@@ -0,0 +1,30 @@
+package com.ssafy.setak.db.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_detail_id")
+    private Long id;
+
+    private String item;
+    private float price;
+    private String blockAddr1;
+    private String blockAddr2;
+    private String blockAddr3;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
