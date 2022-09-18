7a83d52c21c0caa72b4c1db09d4b0eacf2260a3c
마유선 <jasminema@naver.com>
2022-09-18 20:20:35 +0900

    feat: add laundryitem entity

---
+++ backend/src/main/java/com/ssafy/setak/db/entity/LaundryItem.java
@@ -0,0 +1,26 @@
+package com.ssafy.setak.db.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LaundryItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "laundry_item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "laundry_id")
+    private Laundry laundry;
+}
