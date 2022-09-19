d3d00f7a56a72411040d4aa2588dfb5a782129f2
DESKTOP-KVCQHCD\SSAFY <dbwowo@naver.com>
2022-09-19 16:45:56 +0900

    feat: fix userwalletrepository

--- backend/src/main/java/com/ssafy/setak/db/repository/UserWalletRepository.java
+++ backend/src/main/java/com/ssafy/setak/db/repository/UserWalletRepository.java
@@ -1,9 +1,10 @@
 package com.ssafy.setak.db.repository;
 
 import com.ssafy.setak.db.entity.Order;
+import com.ssafy.setak.db.entity.UserWallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserWalletRepository extends JpaRepository<Order, Long> {
+public interface UserWalletRepository extends JpaRepository<UserWallet, Long> {
 }
