5ca93b205fd61c601649c805ce4d072a8378b9ae
김수미 <amysumi@naver.com>
2022-09-19 18:04:14 +0900

    feat: add login page

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { Outlet } from 'react-router-dom';
+import GlobalFont from '../styles/fonts/GlobalFont';
+import Globalstyle from '../styles/fonts/Globalstyle';
 import Header from './layout/Header';
+import Navigation from './layout/Navigation';
 
 const Main: React.FC = () => {
   return (
     <div>
+      <Globalstyle />
+      <GlobalFont />
       <Header />
+      <Navigation />
       <main>
         <Outlet />
       </main>
--- frontend/src/components/user/Login.tsx
+++ frontend/src/components/user/Login.tsx
@@ -1,7 +1,46 @@
+import { Box, Button, Grid, Link, TextField } from '@mui/material';
 import * as React from 'react';
 
 const Login: React.FC = () => {
-  return <div>로그인 페이지 입니다.</div>;
+  return (
+    <div className="login">
+      로그인 페이지 입니다.
+      <Box component="form" noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="id"
+          label="아이디를 입력하세요."
+          name="id"
+          autoComplete="id"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="비밀번호를 입력하세요."
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}>
+          로그인
+        </Button>
+
+        <Link href="#">비밀번호 찾기</Link>
+        <br />
+        <br />
+        <Link href="#">회원가입하기</Link>
+      </Box>
+    </div>
+  );
 };
 
 export default Login;
