71ec9cc85d957eecf68249226ac6ba81acd7c10c
조미곤 <migon99@naver.com>
2022-09-20 12:25:54 +0900

    conflict: resolve conflict

--- frontend/src/components/user/Signup.tsx
+++ frontend/src/components/user/Signup.tsx
@@ -10,6 +10,7 @@ const Signup: React.FC = () => {
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
   const [tosCheck, setTosCheck] = useState(false);
+  const [page, setPage] = useState(1);
 
   const emailChange = (event) => {
     // const regEmail =
@@ -34,38 +35,52 @@ const Signup: React.FC = () => {
     }
   };
 
+  const onClickChange = () => {
+    setPage(2);
+  };
+
   return (
     <div>
       <h2>회원가입 페이지 입니다.</h2>
-      <div className="signup-type">
-        <button type="button" onClick={() => setMode('customer')}>
-          고객 회원가입
-        </button>
-        <button type="button" onClick={() => setMode('ceo')}>
-          사업자 회원가입
-        </button>
-      </div>
-      <FormControl sx={{ width: 315 }}>
-        <InputLabel htmlFor="email">이메일</InputLabel>
-        <Input value={email} onChange={emailChange} />
-      </FormControl>
-      <br />
-      <FormControl sx={{ width: 315 }}>
-        <InputLabel htmlFor="password">비밀번호</InputLabel>
-        <Input type="password" value={password} onChange={passwordChange} />
-      </FormControl>
-      <br />
-      <FormControl sx={{ width: 315 }}>
-        <InputLabel htmlFor="passwordcheck">비밀번호 확인</InputLabel>
-        <Input
-          type="password"
-          value={passwordCheck}
-          onChange={passwordCheckChange}
-        />
-      </FormControl>
-      <br />
-      <Checkbox onClick={onClickTOS} />
-      <Link to="../TOS">세탁클로쓰의 이용약관</Link>에 동의합니다.
+      {page === 1 ? (
+        <div>
+          <div className="signup-type">
+            <button type="button" onClick={() => setMode('customer')}>
+              고객 회원가입
+            </button>
+            <button type="button" onClick={() => setMode('ceo')}>
+              사업자 회원가입
+            </button>
+          </div>
+          <FormControl sx={{ width: 315 }}>
+            <InputLabel htmlFor="email">이메일</InputLabel>
+            <Input value={email} onChange={emailChange} />
+          </FormControl>
+          <br />
+          <FormControl sx={{ width: 315 }}>
+            <InputLabel htmlFor="password">비밀번호</InputLabel>
+            <Input type="password" value={password} onChange={passwordChange} />
+          </FormControl>
+          <br />
+          <FormControl sx={{ width: 315 }}>
+            <InputLabel htmlFor="passwordcheck">비밀번호 확인</InputLabel>
+            <Input
+              type="password"
+              value={passwordCheck}
+              onChange={passwordCheckChange}
+            />
+          </FormControl>
+          <br />
+          <Checkbox onClick={onClickTOS} />
+          <Link to="../TOS">세탁클로쓰의 이용약관</Link>에 동의합니다.
+          <Link to="/">취소</Link>
+          <button type="button" onClick={onClickChange}>
+            다음
+          </button>
+        </div>
+      ) : (
+        <div>두 번째 페이지</div>
+      )}
     </div>
   );
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/App.scss';
-import './styles/User.scss';
-
 import App from './App';
 
 ReactDOM.render(
