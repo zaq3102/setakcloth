cbf0bce9ec4990f134a87f785847beb6a21eb76c
1upright <kompany11@hanmail.net>
2022-09-21 18:14:50 +0900

    feat: update authcontroller - add login

--- backend/src/main/java/com/ssafy/setak/api/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/setak/api/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.ssafy.setak.api.controller;
 
+import com.ssafy.setak.api.request.UserLoginPostReq;
 import com.ssafy.setak.api.response.AuthRes;
 import com.ssafy.setak.api.service.JwtService;
 import com.ssafy.setak.api.service.KakaoService;
@@ -9,6 +10,7 @@ import com.ssafy.setak.db.entity.User;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
@@ -33,13 +35,16 @@ public class AuthController {
     @Autowired
     private CookieUtil cookieUtil;
 
-    @GetMapping("/login")
-    @ApiOperation(value = "카카오 로그인", notes = "카카오로 로그인한다.")
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @GetMapping("/login/kakao")
+    @ApiOperation(value = "고객 카카오 로그인", notes = "고객 카카오 로그인을 한다.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Success"),
             @ApiResponse(code = 403, message = "탈퇴한 회원입니다."),
             @ApiResponse(code = 409, message = "존재하지 않는 회원입니다."),
-            @ApiResponse(code = 500, message = "카카오 로그인 실패")
+            @ApiResponse(code = 500, message = "고객 카카오 로그인 실패")
     })
     public ResponseEntity<? extends AuthRes> kakaoLogin(@RequestParam String code, HttpServletResponse response) {
         try {
@@ -61,8 +66,51 @@ public class AuthController {
 
             return ResponseEntity.status(200).body(AuthRes.of(200, "Success", accessToken, true, user.getId()));
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(AuthRes.of(500, "카카오 로그인 실패", null, false, 0L));
+            return ResponseEntity.status(500).body(AuthRes.of(500, "고객 카카오 로그인 실패", null, false, -1l));
         }
     }
 
+    @PostMapping("/login")
+    @ApiOperation(value = "고객 일반 로그인", notes = "고객 일반 로그인을 한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 401, message = "잘못된 비밀번호입니다."),
+            @ApiResponse(code = 403, message = "탈퇴한 회원입니다."),
+            @ApiResponse(code = 404, message = "존재하지 않는 계정입니다."),
+            @ApiResponse(code = 500, message = "고객 일반 로그인 실패")
+    })
+    public ResponseEntity<AuthRes> login(@RequestBody @ApiParam(value = "로그인 정보", required = true) UserLoginPostReq loginInfo, HttpServletResponse response) {
+        try {
+            String email = loginInfo.getEmail();
+            String pwd = loginInfo.getPwd();
+            User user = userService.getUserByEmail(email);
+
+            if (user.isWithdrawn()) {
+                return ResponseEntity.status(403).body(AuthRes.of(403, "탈퇴한 회원입니다.", null, false, user.getId()));
+            }
+
+            // 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
+            if (passwordEncoder.matches(pwd, user.getPwd())) {
+                Map<String, String> userInfo = new HashMap<>();
+                userInfo.put("id", user.getId() + "");
+
+                String accessToken = jwtService.createAccessToken("user", userInfo, "user");
+                Cookie accessCookie = cookieUtil.addAccessCookie(accessToken);
+                response.addCookie(accessCookie);
+
+                String refreshToken = jwtService.createRefreshToken("user", userInfo, "user");
+                Cookie refreshCookie = cookieUtil.addRefreshCookie(refreshToken);
+                response.addCookie(refreshCookie);
+                // 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
+                return ResponseEntity.status(200).body(AuthRes.of(200, "Success", accessToken, true, user.getId()));
+            }
+
+            // 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
+            return ResponseEntity.status(401).body(AuthRes.of(401, "잘못된 비밀번호입니다.", null, false, -1l));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404).body(AuthRes.of(404, "존재하지 않는 계정입니다.", null, false, -1l));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(AuthRes.of(500, "고객 일반 로그인 실패", null, false, -1l));
+        }
+    }
 }
