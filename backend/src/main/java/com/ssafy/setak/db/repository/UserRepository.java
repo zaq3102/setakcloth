5df97820eb82318162f9a662208f0b64e159147f
1upright <kompany11@hanmail.net>
2022-09-21 17:16:20 +0900

    feat: update userrepository - add findbyemail

--- backend/src/main/java/com/ssafy/setak/db/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/setak/db/repository/UserRepository.java
@@ -5,7 +5,11 @@ import com.ssafy.setak.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByNickName(String nickName);
+
+    Optional<User> findByEmail(String email);
 }
