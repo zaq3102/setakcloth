86ae7036dfe8a628ece3ff043de5107577ec6926
마유선 <jasminema@naver.com>
2022-09-23 23:26:13 +0900

    feat: update laundryitem entity - add builder annotation

--- backend/src/main/java/com/ssafy/setak/db/entity/LaundryItem.java
+++ backend/src/main/java/com/ssafy/setak/db/entity/LaundryItem.java
@@ -1,15 +1,13 @@
 package com.ssafy.setak.db.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LaundryItem {
