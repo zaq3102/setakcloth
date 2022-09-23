0e37fd2b12f4238df03ae9b3cf51e7295a4b8441
마유선 <jasminema@naver.com>
2022-09-23 16:23:48 +0900

    feat: update laundryrepository - add selectAllLaundryOrderByScore

--- backend/src/main/java/com/ssafy/setak/db/repository/LaundryRepository.java
+++ backend/src/main/java/com/ssafy/setak/db/repository/LaundryRepository.java
@@ -28,4 +28,14 @@ public interface LaundryRepository extends JpaRepository<Laundry, Long> {
             "GROUP BY o.laundry " +
             "ORDER BY cnt DESC")
     List<Tuple> selectAllLaundryOrderByOrder(@Param("userId") Long userId);
+
+    @Query("SELECT l, round(6371 * acos(cos(l.address.addrLat * 3.141592653589793 / 180.0) * cos(u.address.addrLat * 3.141592653589793 / 180.0) * cos((u.address.addrLng * 3.141592653589793 / 180.0) - (l.address.addrLng * 3.141592653589793 / 180.0)) + sin(l.address.addrLat * 3.141592653589793 / 180.0) * sin(u.address.addrLat * 3.141592653589793 / 180.0)),1) as distance," +
+            "AVG(o.reviewScore) AS score " +
+            "FROM Laundry l, User u, Order o " +
+            "WHERE u.id = 1 " +
+            "AND l.id = o.laundry.id " +
+            "AND round(6371 * acos(cos(l.address.addrLat * 3.141592653589793 / 180.0) * cos(u.address.addrLat * 3.141592653589793 / 180.0) * cos((u.address.addrLng * 3.141592653589793 / 180.0) - (l.address.addrLng * 3.141592653589793 / 180.0)) + sin(l.address.addrLat * 3.141592653589793 / 180.0) * sin(u.address.addrLat * 3.141592653589793 / 180.0)),1) <= 1.5 " +
+            "GROUP BY o.laundry " +
+            "ORDER BY score DESC")
+    List<Tuple> selectAllLaundryOrderByScore(@Param("userId") Long userId);
 }
