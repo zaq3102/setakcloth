9b5998205580c60f20d85ee0e3d19a5e6e0acdd9
Sumi Kim <sumikim@Sumiui-MacBookAir.local>
2022-09-27 11:13:30 +0900

    fix: merge conflict

---
+++ frontend/src/components/common/KakaoMaps.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    kakao?: any;
+  }
+}
+const { kakao } = window;
+
+const KakaoMaps = () => {
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 2
+    };
+    const map = new kakao.maps.Map(container, options);
+
+    const marker = new kakao.maps.Marker({
+      // 지도 중심좌표에 마커 생성
+      position: map.getCenter()
+    });
+
+    marker.setMap(map);
+  }, []);
+
+  return (
+    <div
+      id="map"
+      style={{
+        width: '450px',
+        height: '300px'
+      }}
+    />
+  );
+};
+
+export default KakaoMaps;
