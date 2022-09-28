9cbc7269c71389daeb411c09404e547100aac394
Sumi Kim <sumikim@Sumiui-MacBookAir.local>
2022-09-28 14:51:32 +0900

    feat: add laundry list sort function

--- frontend/src/components/user/customer/CtmHome.tsx
+++ frontend/src/components/user/customer/CtmHome.tsx
@@ -1,5 +1,4 @@
 import { Button, Dialog, DialogActions, DialogTitle } from '@mui/material';
-
 import * as React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
@@ -13,12 +12,6 @@ const CtmHome: React.FC = () => {
 
   const [detailaddress, setDetailaddress] = useState<string>('17층');
 
-  // const modes = [0, 1, 2, 3, 4];
-
-  const moveToList = () => {
-    navigate('laundrylist');
-  };
-
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -26,12 +19,18 @@ const CtmHome: React.FC = () => {
   const handleClose = () => {
     setOpen(false);
   };
+
   const handleChange = () => {
     setOpen(false);
   };
+
   const [myaddress, setMyaddress] = useState<string>(
     '서울특별시 강남구 역삼동 멀티캠퍼스'
   );
+
+  const handleButton = (mode) => {
+    navigate('./laundrylist', { state: mode });
+  };
 
   return (
     <div>
@@ -61,45 +60,14 @@ const CtmHome: React.FC = () => {
         <br /> <br />
       </div>
       <div className="list-buttons">
-        <div className="list-buttons-up">
-          <Button
-            onClick={moveToList}
-            fullWidth={false}
-            className="list-button-tag-set"
-            sx={{
-              maxWidth: '50%',
-              minWidth: '50%'
-            }}>
-            <img
-              className="laundry-list-img"
-              src="../assets/heart.png"
-              alt="favourite laundry list"
-            />
-            <div className="button-tag-text">즐겨찾기 세탁소 목록</div>
-          </Button>
-          <Button
-            onClick={moveToList}
-            className="list-button-tag-set"
-            sx={{
-              maxWidth: '50%',
-              minWidth: '50%'
-            }}>
-            <img
-              className="laundry-list-img"
-              src="../assets/clock.png"
-              alt="recently used laundry list"
-            />
-            <div className="button-tag-text">최근 이용한 세탁소 목록</div>
-          </Button>
-        </div>
         <div className="list-buttons-down">
           <Button
-            onClick={moveToList}
             className="list-button-tag-set"
             sx={{
               maxWidth: '50%',
               minWidth: '50%'
-            }}>
+            }}
+            onClick={() => handleButton(1)}>
             <img
               className="laundry-list-img"
               src="../assets/map.png"
@@ -109,18 +77,48 @@ const CtmHome: React.FC = () => {
           </Button>
 
           <Button
-            onClick={moveToList}
             className="list-button-tag-set"
             sx={{
               maxWidth: '50%',
               minWidth: '50%'
-            }}>
+            }}
+            onClick={() => handleButton(2)}>
             <img
               className="laundry-list-img"
               src="../assets/star.png"
               alt="highest rate laundry list"
             />
             <div className="button-tag-text">별점 높은 세탁소 목록</div>
+          </Button>
+        </div>
+        <div className="list-buttons-up">
+          <Button
+            className="list-button-tag-set"
+            sx={{
+              maxWidth: '50%',
+              minWidth: '50%'
+            }}
+            onClick={() => handleButton(3)}>
+            <img
+              className="laundry-list-img"
+              src="../assets/heart.png"
+              alt="favourite laundry list"
+            />
+            <div className="button-tag-text">즐겨찾기 세탁소 목록</div>
+          </Button>
+          <Button
+            className="list-button-tag-set"
+            sx={{
+              maxWidth: '50%',
+              minWidth: '50%'
+            }}
+            onClick={() => handleButton(4)}>
+            <img
+              className="laundry-list-img"
+              src="../assets/clock.png"
+              alt="recently used laundry list"
+            />
+            <div className="button-tag-text">최근 이용한 세탁소 목록</div>
           </Button>
         </div>
       </div>
