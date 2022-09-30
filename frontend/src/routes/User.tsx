e522f1ca0c7a0b0d750629a4e476c013ccdb5076
1upright <kompany11@hanmail.net>
2022-09-30 14:54:38 +0900

    feat: update user - add kakao route

--- frontend/src/routes/User.tsx
+++ frontend/src/routes/User.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Signup from '../components/user/Signup';
 import Login from '../components/user/Login';
+import Kakao from '../components/user/Kakao';
 import TOS from '../components/user/TOS';
 import '../styles/User.scss';
 
@@ -11,6 +12,7 @@ const User = () => {
       <Route path="signup" element={<Signup />} />
       <Route path="login" element={<Login />} />
       <Route path="tos" element={<TOS />} />
+      <Route path="kakao/:data" element={<Kakao />} />
     </Routes>
   );
 };
