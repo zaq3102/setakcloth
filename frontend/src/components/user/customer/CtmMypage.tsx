16f8772f5adfc2edb078b65d683a63d4665e9542
조미곤 <migon99@naver.com>
2022-10-02 07:00:20 +0900

    conflict: fix conflict

--- frontend/src/components/user/customer/CtmMypage.tsx
+++ frontend/src/components/user/customer/CtmMypage.tsx
@@ -1,12 +1,5 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Pagination
-} from '@mui/material';
 import * as React from 'react';
+import { Card, CardContent, CardMedia, Chip } from '@mui/material';
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {
@@ -19,26 +12,28 @@ import {
   myorderCtmRequest,
   myReviewRequest
 } from '../../../store/actions/services/orderService';
-import { InfoRequest } from '../../../store/actions/services/userService';
+import {
+  InfoRequest,
+  LaundryLikeRequest
+} from '../../../store/actions/services/userService';
 import '../../../styles/Customer.scss';
 
-const CtmMypage: React.FC = () => {
+const CtmMypage = () => {
   const [clean, setClean] = useState<number>(12340000000000);
   const [point, setPoint] = useState<number>(12340);
   const [openModal1, setOpenModal1] = useState<boolean>(false);
   const [openModal2, setOpenModal2] = useState<boolean>(false);
-  const [pageReview, setPageReview] = useState(1);
-  const [pageOrder, setPageOrder] = useState(1);
   const [userInfo, setUserInfo] = useState('');
   const [reviewList, setReviewList] = useState([]);
   const [orderList, setOrderList] = useState([]);
+  const [likeList, setLikeList] = useState([]);
   const [pending, setPending] = useState(false);
-  // const [profile, setProfile] = useState({ profileImg: '' });
+  const [mode, setMode] = useState(1);
+  const [modeState, setModeState] = useState(-1);
   const navigate = useNavigate();
 
   const getMypage = async () => {
     const result = await InfoRequest();
-
     if (result?.data?.userInfo) {
       setUserInfo(result?.data?.userInfo);
     } else {
@@ -64,176 +59,248 @@ const CtmMypage: React.FC = () => {
     }
   };
 
-  const get = async () => {
-    const result = await gettest('dbwowo');
-    console.log(result);
+  const getMyLikes = async () => {
+    const result = await LaundryLikeRequest();
+    if (result?.data?.laundrys) {
+      setLikeList(result?.data?.laundrys);
+    } else {
+      navigate('/error');
+    }
   };
 
   useEffect(() => {
-    setPending(true);
-
-    get();
     getMypage();
     getMyReviews();
     getMyOrders();
-
-    setTimeout(() => {
-      setPending(false);
-    }, 3000);
+    getMyLikes();
   }, []);
 
-  const pageReviewChange = (event, value) => {
-    setPageReview(value);
-  };
-
-  const pageOrderChange = (event, value) => {
-    setPageOrder(value);
-  };
-
-  const handleOpenModal = (modalType) => {
-    if (modalType === 1) {
-      setOpenModal1(true);
-    } else if (modalType === 2) {
-      setOpenModal2(true);
+  const handleMode = (value) => {
+    setMode(value);
+    if (value === 1) {
+      setModeState(-1);
     }
   };
 
-  const handleClose = (modalType) => {
-    if (modalType === 1) {
-      setOpenModal1(false);
-    } else if (modalType === 2) {
-      setOpenModal2(false);
+  const handleModeState = (value) => {
+    setModeState(value);
+  };
+
+  let content = '';
+  if (mode === 1) {
+    let orderTempList = orderList;
+    if (modeState === 0) {
+      orderTempList = orderList.filter((order) => order.state === 0);
+    } else if (modeState === 1) {
+      orderTempList = orderList.filter((order) => order.state === 1);
+    } else if (modeState === 2) {
+      orderTempList = orderList.filter((order) => order.state >= 2);
     }
-  };
 
-  const handlecharge = async () => {
-    const result = await unlockAccount(userInfo.wallet, '12341234');
-    console.log(result);
-  };
-
-  const onClickItem = (value: string) => {
-    navigate(`./${value}`);
-  };
-
+    content = (
+      <>
+        {orderTempList.map((order) => (
+          <Link to={`../order/${order.orderId}`} key={order.orderId}>
+            <Card
+              sx={{ maxWidth: 250, maxHeight: 250, borderRadius: 10 }}
+              className="ctm-mypage-right-bottom-review">
+              <CardMedia
+                component="img"
+                height="100"
+                image="../assets/laundry1.jpg"
+                alt="green iguana"
+              />
+              <CardContent
+                className="ctm-mypage-right-bottom-review-text"
+                sx={{ maxWidth: 250, maxHeight: 150 }}>
+                {order.orderId}
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+      </>
+    );
+  } else if (mode === 2) {
+    content = (
+      <>
+        {reviewList.map((review, idx) => (
+          <Card
+            sx={{ maxWidth: 250, maxHeight: 250, borderRadius: 10 }}
+            key={idx}
+            className="ctm-mypage-right-bottom-review">
+            <CardMedia
+              component="img"
+              height="100"
+              image="../assets/laundry1.jpg"
+              alt="green iguana"
+            />
+            <CardContent
+              className="ctm-mypage-right-bottom-review-text"
+              sx={{ maxWidth: 250, maxHeight: 150 }}>
+              {review.content}
+            </CardContent>
+          </Card>
+        ))}
+      </>
+    );
+  } else if (mode === 3) {
+    content = (
+      <>
+        {likeList.map((like, idx) => (
+          <Link to={`../${like.laundryId}`} key={like.laundryId}>
+            <Card
+              sx={{ maxWidth: 250, maxHeight: 250, borderRadius: 10 }}
+              className="ctm-mypage-right-bottom-review">
+              <CardMedia
+                component="img"
+                height="100"
+                image="../assets/laundry1.jpg"
+                alt="green iguana"
+              />
+              <CardContent
+                className="ctm-mypage-right-bottom-review-text"
+                sx={{ maxWidth: 250, maxHeight: 150 }}>
+                {like.laundryName}
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+      </>
+    );
+  }
   return (
-    <div className="ctm-my-page">
-      {pending ? (
-        <Loading />
-      ) : (
-        <>
-          <div className="ctm-my-page-left">
-            <div className="ctm-my-info">
-              <div className="ctm-my-info-title">내 정보</div>
-              <div className="ctm-my-info-content">
-                <div className="ctm-my-info-content-right">
-                  <div>
-                    {userInfo.nickName ? userInfo.nickName : '사용자'}님, 오늘도
-                    화이팅!
-                  </div>
-                  <br />
-                  <div>
-                    주소 : {userInfo.addr} {userInfo.addrDetail}
-                  </div>
-                  <br />
-                  <div>(클린 아이콘 들어갈 예정) {userInfo.balance} 클린</div>
-                  <div>(포인트 아이콘 들어갈 예정) {point} 포인트</div>
-                  <button
-                    type="button"
-                    className="ctm-my-page-btn"
-                    onClick={() => handleOpenModal(1)}>
-                    충전
-                  </button>
-                </div>
-              </div>
-              <Dialog open={openModal1} onClose={() => handleClose(1)}>
-                <div className="ctm-item-modal">
-                  <DialogTitle>클린 충전하기</DialogTitle>
-                  <DialogContent>
-                    <div>클린 충전 내용</div>
-                  </DialogContent>
-                  <DialogActions>
-                    <Button onClick={() => handleClose(1)} color="color2">
-                      취소
-                    </Button>
-                    <Button
-                      onClick={handlecharge}
-                      variant="contained"
-                      color="color2">
-                      충전하기
-                    </Button>
-                  </DialogActions>
-                </div>
-              </Dialog>
+    <div className="ctm-mypage">
+      <div className="ctm-mypage-left">
+        <div className="ctm-mypage-left-top">
+          <img
+            className="ctm-mypage-left-top-profile"
+            src="../assets/user.png"
+            alt="user-img"
+          />
+        </div>
+        <div className="ctm-mypage-left-bottom">
+          <div className="ctm-mypage-left-bottom-content">
+            <div className="ctm-mypage-left-bottom-nickname">
+              {userInfo.nickName ? userInfo.nickName : '닉네임을 바꿔주세요.'}
             </div>
-            <div className="ctm-my-review-list">
-              <div className="ctm-my-review-list-title">
-                내가 쓴 세탁소 리뷰
-              </div>
-              <div>총 {reviewList.length}개의 리뷰를 작성하였습니다.</div>
-              <div className="ctm-my-review-list-content">
-                <div className="ctm-my-review-detail">
-                  {reviewList
-                    .slice((pageReview - 1) * 3, pageReview * 3)
-                    .map((review, idx) => (
-                      <Link key={idx} to="/customer/mypage">
-                        <div className="ctm-my-review">{review.content}</div>
-                      </Link>
-                    ))}
-                </div>
-                <div className="ctm-pagination">
-                  <Pagination
-                    count={Math.ceil(reviewList.length / 3)}
-                    page={pageReview}
-                    // variant="outlined"
-                    color="color2"
-                    className={`${
-                      reviewList.length === 0
-                        ? 'ctm-no-pagination'
-                        : 'ctm-pagination'
-                    }`}
-                    onChange={pageReviewChange}
-                  />
-                </div>
-              </div>
+            <div className="ctm-mypage-left-bottom-address">
+              {userInfo.addr} {userInfo.addrDetail}
             </div>
           </div>
-          <div className="ctm-my-order-list">
-            <div className="ctm-my-order-list-title">나의 세탁 내역</div>
-            <div>총 {orderList.length}번의 세탁을 하셨습니다.</div>
-            <div className="ctm-my-order-list-content">
-              <div className="ctm-my-order-detail">
-                {orderList
-                  .slice((pageOrder - 1) * 4, pageOrder * 4)
-                  .map((order) => (
-                    <Link to={`../order/${order.orderId}`} key={order.orderId}>
-                      <div className="ctm-my-order">
-                        <div>{order.orderId}</div>
-                        <div>{order.laundryName}</div>
-                        <div>{order.totalPrice}</div>
-                        <div>{order.orderType}</div>
-                        <div>{order.state}</div>
-                      </div>
-                    </Link>
-                  ))}
-              </div>
-              <div className="ctm-pagination">
-                <Pagination
-                  count={Math.ceil(orderList.length / 4)}
-                  page={pageOrder}
-                  // variant="outlined"
-                  color="color2"
-                  className={`${
-                    orderList.length === 0
-                      ? 'ctm-no-pagination'
-                      : 'ctm-pagination'
-                  }`}
-                  onChange={pageOrderChange}
-                />
-              </div>
-            </div>
+          <div className="ctm-mypage-left-bottom-chips">
+            <Chip
+              className="ctm-mypage-left-bottom-chip"
+              label="닉네임 변경"
+              style={{
+                height: 40,
+                width: 140,
+                background: 'linear-gradient(#e66465, #FFD6EC)'
+              }}
+              // onClick={() => handleMode(0)}
+            />
+            <Chip
+              className="ctm-mypage-left-bottom-chip"
+              label="주소 변경"
+              style={{
+                height: 40,
+                width: 140,
+                background: 'linear-gradient(#e66465, #FFD6EC)'
+              }}
+              // onClick={() => handleMode(0)}
+            />
+            <Chip
+              className="ctm-mypage-left-bottom-chip"
+              label="충전하기"
+              style={{
+                height: 40,
+                width: 140,
+                background: 'linear-gradient(#e66465, #FFD6EC)'
+              }}
+              // onClick={() => handleMode(0)}
+            />
           </div>
-        </>
-      )}
+        </div>
+      </div>
+      <div className="ctm-mypage-right">
+        <div className="ctm-mypage-right-top">
+          <Chip
+            className="ctm-mypage-right-top-chip"
+            label="나의 주문"
+            style={{
+              height: 40,
+              width: 140,
+              marginRight: 20,
+              background: 'linear-gradient(#e66465, #FFD6EC)'
+            }}
+            onClick={() => handleMode(1)}
+          />
+          <Chip
+            className="ctm-mypage-right-top-chip"
+            label="나의 리뷰"
+            style={{
+              height: 40,
+              width: 140,
+              marginRight: 20,
+              background: 'linear-gradient(#e66465, #FFD6EC)'
+            }}
+            onClick={() => handleMode(2)}
+          />
+          <Chip
+            className="ctm-mypage-right-top-chip"
+            label="나의 즐겨찾기"
+            style={{
+              height: 40,
+              width: 140,
+              marginRight: 20,
+              background: 'linear-gradient(#e66465, #FFD6EC)'
+            }}
+            onClick={() => handleMode(3)}
+          />
+        </div>
+        {mode === 1 ? (
+          <div className="ctm-mypage-right-medium">
+            <Chip
+              className="ctm-mypage-right-medium-chip"
+              label="전체"
+              style={{
+                height: 20,
+                width: 140
+              }}
+              onClick={() => handleModeState(-1)}
+            />
+            <Chip
+              className="ctm-mypage-right-medium-chip"
+              label="수락 대기중"
+              style={{
+                height: 20,
+                width: 140
+              }}
+              onClick={() => handleModeState(0)}
+            />
+            <Chip
+              className="ctm-mypage-right-medium-chip"
+              label="세탁중"
+              style={{
+                height: 20,
+                width: 140
+              }}
+              onClick={() => handleModeState(1)}
+            />
+            <Chip
+              className="ctm-mypage-right-medium-chip"
+              label="완료"
+              style={{
+                height: 20,
+                width: 140
+              }}
+              onClick={() => handleModeState(2)}
+            />
+          </div>
+        ) : (
+          <div className="ctm-mypage-right-medium" />
+        )}
+        <div className="ctm-mypage-right-bottom">{content}</div>
+      </div>
     </div>
   );
 };
